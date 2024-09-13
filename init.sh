8ac447620a5d222c7603f52f2600ab7e4d2a97bc
Michaelomeister <mike@makarov.pw>
2024-09-13 10:36:37 +0200

    add custom hostname for tailscale

--- init.sh
+++ init.sh
@@ -3,10 +3,11 @@
 login_server=$(if [ -n "$TAILSCALE_LOGIN_SERVER" ]; then echo "--login-server=$TAILSCALE_LOGIN_SERVER"; else echo ""; fi)
 accept_routes=$(if [ "$TAILSCALE_ACCEPT_ROUTES" = "true" ]; then echo "--accept-routes=true"; else echo "--accept-routes=false"; fi)
 accept_dns=$(if [ "$TAILSCALE_ACCEPT_DNS" = "true" ]; then echo "--accept-dns=true"; else echo "--accept-dns=false"; fi)
+hostname=$(if [ -n "$TAILSCALE_HOSTNAME" ]; then echo "--hostname=$TAILSCALE_HOSTNAME"; else echo ""; fi)
 
 #Start tailscaled and connect to tailnet
 /usr/sbin/tailscaled --state=/var/lib/tailscale/tailscaled.state >> /dev/stdout &
-/usr/bin/tailscale up --auth-key $TAILSCALE_AUTH_KEY $login_server $accept_routes $accept_dns >> /dev/stdout &
+/usr/bin/tailscale up --auth-key $TAILSCALE_AUTH_KEY $login_server $accept_routes $accept_dns $hostname >> /dev/stdout &
 
 #Check for and or create certs directory
 mkdir -p /root/derper/$TAILSCALE_DERP_HOSTNAME
